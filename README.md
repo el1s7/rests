049450d85367f882f2a157edfea761438e08d232
elis-k <me@elis.cc>
2020-10-07 18:11:54 +0200

    First version

---
+++ README.md
@@ -0,0 +1,189 @@
+# Wrape
+[![license](https://img.shields.io/github/license/elis-k/wrape)](https://github.com/elis-k/wrape/blob/master/LICENSE)
+[![npm](https://img.shields.io/npm/v/wrape)](https://www.npmjs.com/package/wrape)
+[![npm](https://img.shields.io/npm/dw/wrape)](https://www.npmjs.com/package/wrape)
+
+API wrapper to organize and simplify API & Requests — OOP-ish way.
+
+## Features  
+- No dependencies, using Fetch API
+- Multiple API categories & subcategories
+- Elegant parameter handling
+- Universal support, small size
+& much more.
+
+## Installation
+`npm i wrape`
+
+## Quick  Documentaion
+### Usage
+```javascript
+import Wrape from 'wrape';
+import {api_configuration,api_options} from './config';
+const api = Wrape(api_configuration,api_options);
+```
+### API Configuration 
+- #### Categories
+   An API category is an object consisting of [Endpoint Object](#endpoint-object)s or subcategories.
+
+ - #### Endpoint Object
+   - **`method`**: The request method ,GET,POST etc.
+   - **`path`**: The request path, can also contain named parameters,  check [exmaple](#another-example) below. 
+   - **`enctype`**: The body encode type for *requests that have body* parameters:
+	   - `multipart/form-data` *(default)*
+	   - `application/x-www-form-urlencoded`
+   - **`params`**: An object consisting of [Params Object](#params-object)s.
+
+- #### Params Object
+
+  - **`required`**: If the param is required or not, error will be thrown if required params are not specified.
+  - **`validate`**: Regular expression to test, if doesn't pass, error will be thrown.
+  - **`help`** : An error message to throw if param is not valid, or required and not specified.
+  - **`format`**: A function to format the parameter value.
+  - **`default`**: A default value when value for this param was not specified.
+  - **`location`**: The location where this parameter will be in http request fields, it can be:
+	 - `body` the param will be encoded in body as form data *(default for POST request)*
+	 - `query` the param will be URL encoded in URL query *(default for GET request)*
+	 - `headers` the param will be set in request headers
+	 - `path` the param will be set in request path, you must declare the named parameters in   path, check [exmaple](#another-example) below. 
+By default is `query` for GET requests and `body` for POST requests
+  - **`name`**: By default the param name is the key of the Param Object. Here you can specify another name for the param, though still the key will be used for reference when calling endpoint and passing param.
+
+### API Options
+- Global Options Object
+	- **`base`**: The base URL for this API, will be prepended to each request path, default is empty.
+	- **`headers`**: The global headers to add to all requests.
+	- **`params`**: The global param values for all categories and endpoints.
+	- **`constructed`**: When not enabled, you can't initalize new category objects with `new` command, instead you can  initalize them with a `new` function, i.e `api.category.new({})` instead of `new api.category({})`.  (default: true)
+	- **`agent`**: You can use this option to set proxy if you're using node-fetch.
+## Examples
+
+#### Basic Example
+```javascript
+import Wrape from 'wrape';
+
+(async function(){
+
+//The JSON API Configuration object that Wrape uses to create the API model 
+const api_config = {
+	user:{
+		info:{
+			path: '/user/info',
+			method: 'GET',
+			params:{
+				id:{
+					required: true,
+					help: "The user ID is required.",
+					validate: "^[0-9]+$",
+				},
+				authorization:{
+					required: true,
+					help: "The user authorization token is required.",
+					location: "headers"
+				}
+			}
+		}
+		/* You can add other endpoints for the user category
+		edit:{...}
+		*/
+	}
+	/* You can add other categories 
+	analytics:{...}
+	*/
+	
+}
+const api = Wrape(api_config,{base: 'https://httpbin.org/anything'});
+const User = new api.user({authorization: 'login_token'}); /* You can set default params values for a category by initalizing new object */
+var userInfo = await User.info({id: 1});
+var userInfo = await api.user.info({id: 1,authorization:'login_token'}) /* Without initalizing */
+
+})
+
+
+```
+
+In the example above, we have have created a `user` category with a `info` endpoint. 
+You can call endpoint by passing an object of params, if params are invalid or required it will throw an error with the param help message.
+
+Categories can be with initalized with params that are readable by each endpoint of that category. You can also call the endpoint directly without initalization.
+#### Another Example
+```javascript
+import Wrape from 'wrape';
+
+(async function(){
+
+const api_configuration = {
+	analytics:{
+			log:{
+				path: '/analytics/log',
+				method:'GET',
+				params:{}
+			}
+	},
+	user:{
+		profile:{
+			path: '/profile/:id',
+			method: 'GET',
+			params:{
+				id:{
+					required: true,
+					help: "The user ID is required.",
+					validate: "^[0-9]+$",
+					location: "path"
+				},
+				key:{
+					name: 'Authorization',
+					required: true,
+					help: "The user authorization token is required.",
+					validate: "^Bearer (.*?)$",
+					location: "headers"
+				}
+			}
+		},
+		comment:{
+			path: '/user/feed',
+			method: 'POST',
+			params:{
+				comment:{
+					required: true,
+					help: "The comment text is required.",
+				},
+				key:{
+					name: 'Authorization',
+					required: true,
+					help: "The user authorization token is required.",
+					validate: "^Bearer (.*?)$",
+					location: "headers"
+				}
+			}
+		}
+		
+	}
+
+}
+
+const api = Wrape(api_configuration,{
+	base: 'https://example.com',
+});
+
+//Call Analytics Log Endpoint
+api.analytics.log();
+
+//Initialize User category and set the key 
+const user = new api.user({key: "Bearer ..."});
+
+
+user.comment({
+	comment: 'This is a comment.'
+}).then((res)=> console.log(res))
+}).catch((err)=> console.log(err));
+
+
+try{
+	var userInfo = await user.info({id:'1'});
+}
+catch(err){ console.log(err)}	
+
+});
+
+```
---
+++ index.js
@@ -0,0 +1,232 @@
+function Resify(endpoints,global_options){
+	
+	//Parse Global Options
+	endpoints = endpoints || {};
+	
+	var default_global = {
+		"base": "",
+		"headers":{},
+		"params":{},
+		"agent": false, //node-fetch, able to add proxy
+		"constructed": true
+	}
+	
+	global_options = Object.assign(default_global,global_options);
+	
+	
+
+	function ajax(url, options,type) {
+		return fetch(url, options)
+			.then(async function(res){
+				if (!res.ok) {
+					return Promise.reject(res);
+				}
+				var content_type = res.headers.get('Content-Type');
+				var resobj = {
+					'status_code': res.status,
+					'status': res.status,
+					'statusText': res.statusText,
+					'headers': res.headers,
+				}
+				if(content_type && (new RegExp("application\/json")).test(content_type)){resobj['json'] = await res.json();}
+				else if(content_type && (new RegExp("text\/")).test(content_type)){resobj['text'] = await res.text();}
+				else{resobj['arrayBuffer'] = await res.arrayBuffer()}
+				
+				return resobj
+			});
+	}
+	
+	function emptyConstructor(){
+		return function(def_values){
+			this.def_values = def_values;
+			return this;
+		}
+	}
+	function newSimulateObject(){
+		return function(def_values){
+			var nw = Object.assign({},this);
+			delete nw['new'];
+			nw._def_values = def_values;
+			return nw;
+		}
+	}
+	
+	function initalizer(){
+		//Constructor function
+		if(global_options.constructed){
+			return emptyConstructor();
+		}
+		
+		//Not a constructor function object, but it has a function key to initalize new object. (simulate 'new' command)
+		return {
+			'new': newSimulateObject()
+		}
+		
+	}
+	
+	function isNull(value){
+		return value === null || value === undefined;
+	}
+	
+	function isEmptyIterable(iterable){
+		for (var _ of iterable) {
+			return false;
+		}
+	
+		return true;
+	};
+	function escapeRegExp(string) {
+		return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+	}
+	
+
+	
+	function fetcher(request){
+		
+		request.method = (typeof request.method === "string" ? request.method.toUpperCase() : "GET");
+		
+		
+		var allowed_param_locations = ["headers","body","query","path"];
+		var allowed_param_enctypes = ['multipart/form-data','application/x-www-form-urlencoded'];
+		request.enctype = ((request.enctype && allowed_param_enctypes.includes(request.enctype)) ? request.enctype : allowed_param_enctypes[0]);
+		
+		var def_param_locations = {
+			'POST': 'body',
+			'GET': 'query',
+		}
+		
+		
+		return async function(params){
+			var url = `${global_options.base}${request.path}`;
+			var options = {
+				method: request.method,
+				headers: global_options.headers,
+				agent: global_options.agent
+			}
+			
+			
+			request_params = Object.keys(request.params);
+			argument_params = Array.from(arguments);
+			
+			//Parse the params object
+			//Support passing parameters like arguments, instead of object. (Not recommended for multiple elements)
+			if(argument_params.length > 1){
+				params = argument_params.reduce(function(o,k,i){
+					if(!request_params[i]){
+						return o;
+					}
+					o[request_params[i]] = k;
+					return o;
+				},{});
+			}
+			else if(argument_params.length == 1 && request_params.length == 1 && typeof argument_params[0] !== "object"){
+				params = {
+					[request_params[0]]: argument_params[0]
+				}
+			}
+			else{
+				params = params || {};
+			}
+			
+	
+			this.def_values = this.def_values || {};
+			var body =  ((request.enctype == allowed_param_enctypes[0]) ? new FormData() : new URLSearchParams());
+			var query = new URLSearchParams();
+			
+	
+			for (var param_name in request.params){
+				var param = request.params[param_name];
+				var param_value = params[param_name] || this.def_values[param_name] || global_options.params[param_name] || param.default;
+				var param_dest = param.name || param_name;
+				
+				//Required Param or not
+				if(param.required && isNull(param_value)){ throw new Error(param.help || `The '${param_name}' field is required.`);}
+	
+				//Skip, not required
+				if(isNull(param_value)) continue;
+				
+				//Formatter function?
+				if(typeof param.format === "function"){param_value = param.format(param_value);}
+				
+				//Validate
+				if(param.validate && !(new RegExp(param.validate).test(param_value))){ throw new Error(param.help || `The '${param_name}' field is invalid.`);}
+	
+				//Location
+				var param_location = (typeof param.location === "string" ? param.location.toLowerCase() : def_param_locations[options.method]);
+				if(!param_location || !allowed_param_locations.includes(param_location)){ throw new Error(`Invalid location for '${param_name}' field.`);}
+				
+	
+				if(param_location == "headers"){
+					options['headers'] = options['headers'] || {};
+					options['headers'][param_dest] = param_value;
+					continue;
+				}
+				if(param_location == "body"){
+					body.append(param_dest,param_value);
+					continue;
+				}
+				if(param_location == "query"){
+					query.append(param_dest,param_value);
+					continue;
+				}
+				if(param_location == "path"){
+					url = url.replace(new RegExp(`\:${escapeRegExp(param_name)}`),param_value);
+				}
+			}
+			query = query.toString();
+			
+			if(query){
+				url = `${url}?${query}`;
+			}
+	
+			if(!isEmptyIterable(body.keys())){
+				options['body'] = ((request.enctype == allowed_param_enctypes[0]) ? body : body.toString());
+			}
+	
+			return ajax(url,options);
+		}
+	}
+	
+	function ocreater(root,tree,ne=false){
+		for(var category in tree){
+			var category_tree = tree[category];
+			if(category_tree.hasOwnProperty('path')){
+				var request = category_tree;
+				root._ne = true; //Not empty
+				
+				//Skip duplicate keys in main object root, but still add to prototype.
+				if(typeof root[category] !== "undefined"){
+						console.warn(`Skipping ${category} as it confilicts with another key in the object, avoid using keys like ('name','length','caller','apply',...) because they are reserved constructor words.`);
+						if(global_options.constructed){
+							root.prototype[category] = fetcher(request);
+						}
+						continue;
+				}
+				
+				root[category] = fetcher(request);
+				
+				//Constructor function
+				if(global_options.constructed){
+					root.prototype[category] = root[category]
+				}
+			}
+			else{
+				root[category] = ocreater(initalizer(),category_tree)
+			}
+		}
+		return root._ne || ne ? root : {};
+	}
+	return ocreater(initalizer(),endpoints,true);
+}
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.Resify = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
+    return Resify;
+}));
