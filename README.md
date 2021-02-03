aeb6b3e31c08fc03d410d8a8e6dd18fa86ba8580
elis-k <me@elis.cc>
2021-02-03 19:37:04 +0100

    improve code & update doc

--- README.md
+++ README.md
@@ -1,69 +1,94 @@
+  
 
 # Wrape
-[![license](https://img.shields.io/github/license/elis-k/wrape)](https://github.com/elis-k/wrape/blob/master/LICENSE)
-[![npm](https://img.shields.io/npm/v/wrape)](https://www.npmjs.com/package/wrape)
-[![npm](https://img.shields.io/npm/dw/wrape)](https://www.npmjs.com/package/wrape)
 
-Easily generate API client's SDK — organize and simplify API Requests — OOP-ish way.
+[![license](https://img.shields.io/github/license/elis-k/wrape)](https://github.com/elis-k/wrape/blob/master/LICENSE) [![npm](https://img.shields.io/npm/v/wrape)](https://www.npmjs.com/package/wrape) [![npm](https://img.shields.io/npm/dw/wrape)](https://www.npmjs.com/package/wrape)
 
-## Features  
+Easily generate API client's SDK — organize and simplify API Requests.
+If you find yourself repeating HTTP requests code blocks, then this library is for you.
+  
+  ### Transform this
+ ```javascript
+fetch("https://example.com/login",{
+	'method': 'POST',
+	'headers': {
+		'Content-Type': 'application/x-www-form-urlencoded'
+	},
+	'data': 'user=test&password=test'
+}).then((res) => (if(!res.ok){ throw new Error("error")})
+.then((res) => res.json())
+.catch((err) => console.warn)
+.then(data)=> (console.log("Finally the response")));
+```
+
+### Into this
+
+```javascript
+api.login({
+	user: 'test',
+	password: 'test'
+})
+.then((data)=>(console.log(`Logged in!`)));
+
+```
+
+## Features
+
 - No dependencies, using Fetch API
+
 - Multiple API categories & subcategories
+
 - Elegant parameter handling
+
 - Universal support, small size (2.9KB)
+- For Browsers & Node.js
 
-& much more.
-
+  
 ## Installation
+
 `npm i wrape`
 
+  
+  
 
 ## Basic Example
-```javascript
-import Wrape from 'wrape';
 
-const api_config = {
+You start by writing a JSON schema of all your API endpoints. 
+You can split requests into categories and subcategories.
+```javascript
+import  Wrape  from  'wrape';
+
+const endpoints = {
 	user:{
 		login:{
-			path: '/user/login',
-			method: 'POST',
+			path:  '/user/login',
+			method:  'POST',
 			params:{
 				username:{
-					required: true,
+					required:  true,
 				},
 				password: {
-					required: true,
-					help: "The password must be at least 8 characters.",
-					validate: (password) => { return password.length >= 8;}
+					required:  true,
+					help:  "The password must be at least 8 characters.",
+					validate: (password) => { return  password.length >= 8;}
+
 				}
 			}
-			
 		}
 	}
 }
 
-const api = Wrape(api_config,{base: 'https://example.com'});
+const  api = Wrape(api_config,{ base:  'https://example.com'});
 
-//Wrape Validation Error
-api.user.login({username: 'john', password:'0'})
-.catch((err) => {
+api.user.login({username:  'john', password:'short'}).catch((err) => {
 	console.log(err.message); //The password must be at least 8 characters.
-	console.log(err.param); //password
-	
 });
 
-
-//HTTP Status Error
-api.user.login({username: 'john', password:'wrong_password'})
-.then((res)=> {
-	console.log(res.status); //200
-	console.log(res.json.message); //Logged in successfully.
-})
+api.user.login({username:  'john', password:'wrong_password'})
 .catch((res) => {
-	console.log(res.status); //401
 	console.log(res.json.message); //User password is invalid.
+	console.log(res.status); //401
 });
-	
 ```
 ## Quick  Documentaion
 
@@ -74,39 +99,48 @@ api.user.login({username: 'john', password:'wrong_password'})
 
 #### Endpoint Object
   - **`method`**: The request method ,GET,POST etc.
-  - **`path`**: The request path, can also contain named parameters,  check [exmaple](#another-example) below. 
-  - **`enctype`**: The body encode type for *requests that have body* parameters:
-	 - `multipart/form-data` *(default)*
-	 - `application/x-www-form-urlencoded`
-	 - `application/json`
+  - **`path`**: The request path or full URL, which can also contain named parameters,  check [exmaple](#another-example) below. 
+  - **`enctype`**: The body encode type for \*only for *requests that have body* parameters:
+	 - **`form`**` (multipart/form-data)` *(default)*
+	 - **`url`**` (application/x-www-form-urlencoded)`
+	 - **`json`**` (application/json)`
+	 - **`text`**` (text/plain)`
   - **`params`**: An object consisting of [Params Object](#params-object)s.
-  - **`response`**: A function for formatting the response.
+  - **`response`**: A hook function for modifying the response.
+  - **`request`**: A hook function for modifying the request.
 
 #### Params Object
- - **`name`**: By default the param name is the object key. Here you can specify another name for the param which will be sent on the request, though still the key will be used for reference when calling endpoint and passing param.
- - **`required`**: If the param is required or not, error will be thrown if required params are not specified.
+ - **`name`**: By default the param name is the Param object key. 
+ - **`required`**: If this param is required or not.
  - **`format`**: A function to format the parameter value.
  - **`validate`**: Validate the param values, it can be:
      - A Regular expression string.
      - A function, that returns a boolean.
- - **`help`** : An error message to throw if param is not valid, or required and not specified.
- - **`default`**: A default value when value for this param was not specified.
+ - **`help`** : An error message to throw if param is not valid, or required.
+ - **`default`**: A default value for this param.
  - **`location`**: The location where this parameter will be in http request fields, it can be:
-     - `body` the param will be encoded in body as form data *(default for POST request)*
-     - `query` the param will be URL encoded in URL query *(default for GET request)*
-     - `headers` the param will be set in request headers
-     - `path` the param will be set in request path, you must declare the named parameters in   path, check [exmaple](#another-example) below. 
+     - **`body`** the param will be encoded in body as form data *(default for POST request)*
+     - **`query`** the param will be URL encoded in URL query *(default for GET request)*
+     - **`headers`** the param will be set in request headers
+     - **`path`** the param will be set in request path, you must declare the named parameters in   path, check [exmaple](#another-example) below. 
 
 ### Global Options
-This is the global options you set when you initalize Wrape, `Wrape(api_config,global_options)`
-  - **`base`**: The base URL for this API, will be prepended to each request path, default is empty.
-  - **`defaultHeaders`**: The global headers to add to all requests.
-  - **`defaultParamValues`**: An object of `{param_key : param_value}`, useful for setting default values for all categories and endpoints.
-  - **`parseResponse`**: If disabled the response object will be the default Fetch API response object. *(default true)*
-  - **`agent`**: You can use this option to set proxy if you're using node-fetch. 
-	
+This is the global options you set when you initalize Wrape, these options can be overridden by Category options.
+
+`Wrape(endpoints,global_options)`
+  - **`base`**: The base Origin for all endpoints, will be prepended to each request path, default is empty.
+  - **`headers`**:  Append Headers to all requests
+  - **`values`**: An object of `{param_key : param_value}`, useful for setting default values for all categories and endpoints.
+   - **`sender`**: Use a custom function to send request -> `custom_fetch(url, options, wrape_options, response_middleware?)`
+   - **`request_middleware`**:  Global hook pre-request function
+   - **`response_middleware`**: Global hook on response function
+  - **`fetch_parse`**: Parse Fetch Response (await JSON body\await body text) *(default true)*
+  - **`fetch_error_handler`**:  Function to handle fetch errors
+  - **`fetch_agent`**: You can use this option to set proxy if you're using node-fetch. 
+  
+  
 ### Responses
-The response of request is parsed based by default, returning an object like this:
+The response of request is parsed based when `fetch_parse` option is true, returning an object like this:
 ```javascript
 {
 	status: 200,
@@ -115,151 +149,59 @@ The response of request is parsed based by default, returning an object like thi
 	json: {'message': 'success'},
 }	
 ```
-Depending on the content type, you can get the body as `text`,`json` or `arrayBuffer`.
-
-HTTP statuses other than OK, with throw an promise reject error with the response object.
-Errors can be HTTP status errors, or Wrape errors which are thrown when the param doesn't pass custom validation.
-The Wrape error object has the message of the param `help` and also a `param` property which holds the param name for reference.
+Depending on the content type, you can get the body as `text` or `json`.
 
 
-## Examples
+## Advanced Example
 
-#### Another Example
-Firstly we create the API object using the API configuration.
+In this example we declare a user category and set it's options with the special `$options` key.
+We also declare a `information` endpoint which contains `path` parameters.
 ```javascript
 import Wrape from 'wrape';
 
-const api_config = {
+const endpoints = {
 	user:{
-		info:{
-			path: '/user/info',
-			method: 'GET',
-			params:{
-				id:{
-					required: true,
-					help: "The user ID is required.",
-					validate: "^[0-9]+$",
-				},
-				authorization:{
-					required: true,
-					help: "The user authorization token is required.",
-					location: "headers"
+		//This $options: A special key which overrides Global Options for this category and it's subcategories
+		$options:{
+			base: 'https://example.com/login'
+		},
+		
+		profile:{
+			information:{
+				path: '/profile/:id',
+				method: 'GET',
+				params:{
+					id:{
+						required: true,
+						help: "The user ID is required.",
+						validate: "^[0-9]+$",
+						location: "path"
+					},
+					key:{
+						name: 'Authorization',
+						required: true,
+						help: "The user authorization token is required.",
+						validate: "^Bearer (.*?)$",
+						location: "headers"
+					}
 				}
-			}
-		}
-		/* You can add other endpoints for the user category
-		edit:{...}
-		*/
+			},
+			edit: {...} //Other Endpoint/Subcategories for this category
+		}		   
 	}
-	/* You can add other categories 
-	analytics:{...}
-	*/
-	
 }
-const api = Wrape(api_config,{base: 'https://httpbin.org/anything'});
+const api = Wrape(api_config);
 ```
-Now we can simply call any endpoint like this
+#### Alternative Category Options
+You can also set options for a category by initializing it with `set` function.  This method doesn't work with subcategories.
 ```javascript
-api.user.info({id: 1,authorization:'login_token'})
-.then((res) => console.log(res));
-```
+const Profile = new api.user.profile.set({
+	values: {
+		key: 'default_auth_token'
+	}
+}); 
 
-You can initalize a new API object and set param values
-```javascript
-const User = new api.user.set({authorization: 'login_token'}); 
-User.info({id: 1}).then((res) => {
+Profile.information({id: 1}).then((res) => {
 	console.log(res.json);
 });
-```
-
-Also you can still set or modify param values on an initalized object
-```javascript
-User.set({authorization: 'update_login_token'});
-```
-
-In the example above, we have have created a `user` category with a `info` endpoint. 
-You can call endpoint by passing an object of params, if params are invalid or required it will throw an error with the param help message.
-Categories can be initalized by using the `set` function to store param values, the param values are readable by each endpoint of that category. You can also call the endpoint directly without initalization.
-
-#### And another one
-An example of passing params to the URL Path.
-```javascript
-import Wrape from 'wrape';
-
-(async function(){
-
-const api_configuration = {
-	analytics:{
-			log:{
-				path: '/analytics/log',
-				method:'GET',
-				params:{}
-			}
-	},
-	user:{
-		profile:{
-			path: '/profile/:id',
-			method: 'GET',
-			params:{
-				id:{
-					required: true,
-					help: "The user ID is required.",
-					validate: "^[0-9]+$",
-					location: "path"
-				},
-				key:{
-					name: 'Authorization',
-					required: true,
-					help: "The user authorization token is required.",
-					validate: "^Bearer (.*?)$",
-					location: "headers"
-				}
-			}
-		},
-		comment:{
-			path: '/user/feed',
-			method: 'POST',
-			params:{
-				comment:{
-					required: true,
-					help: "The comment text is required.",
-				},
-				key:{
-					name: 'Authorization',
-					required: true,
-					help: "The user authorization token is required.",
-					validate: "^Bearer (.*?)$",
-					location: "headers"
-				}
-			}
-		}
-		
-	}
-
-}
-
-const api = Wrape(api_configuration,{
-	base: 'https://example.com',
-});
-
-//Call Analytics Log Endpoint
-api.analytics.log();
-
-//Initialize User category and set the key 
-const user = new api.user.set({key: "Bearer ..."});
-
-
-user.comment({
-	comment: 'This is a comment.'
-}).then((res)=> console.log(res))
-}).catch((err)=> console.log(err));
-
-
-try{
-	var userInfo = await user.info({id:'1'});
-}
-catch(err){ console.log(err)}	
-
-});
-
 ```
--- wrape.js
+++ wrape.js
@@ -1,26 +1,92 @@
 /*!
- * Wrape v1.2.5
+ * Wrape v1.2.6
  * Author: Elis <contact@elis.cc>
  * License: MIT
  */
 
-function Wrape(endpoints,global_options){
+
+/**
+ * Initialize
+ * @param {Fetch} fetch Fetch API
+ * @param {FormData} FormData FormData API
+ * @param {Object} endpoints 
+ * @param {Object|*} global_options 
+ */
+
+function Wrape(fetch, FormData, endpoints, global_options){
 	
-	//Parse Global Options
+	/**
+	 * Initalization options
+	 */
 	endpoints = endpoints || {};
 	
 	var default_global = {
 		"base": "",
-		"defaultHeaders":{},
-		"defaultParamValues":{},
-		"parseResponse": true,
-		"agent": false, //node-fetch, able to add proxy
+		"headers":{}, //Append Headers to All Requests
+		"values": {}, //Store default Values for All Params (Key: Value)
+		"sender": false, //Use a custom function to send request -> custom_fetch(url, options, wrape_options, response_middleware?)
+		"request_middleware": null, //Pre-Request Middleware | Can Modify Request (Function)
+		"response_middleware": null, //After-Response Middleware | Can Format Response (Function)
+
+		"fetch_parse": true, //Parse Fetch Response (await JSON body\await body text) (bool)
+		"fetch_agent": false, //node-fetch only, able to add proxy,
+		"fetch_error_handler": false, //Function to handle fetch erros, (Function)
 	}
 	
-	global_options = Object.assign(default_global,global_options);
+	global_options = Object.assign(default_global, global_options);
 	
 
-	/* Some Functions */
+	/**
+	 * Constants
+	 */
+
+	const def_param_enctypes = {
+		"form": "multipart/form-data",
+		"url": "application/x-www-form-urlencoded",
+		"json": "application/json",
+		"text": "text/plain"
+	};
+	const allowed_param_enctypes = Object.values(def_param_enctypes);
+
+	const allowed_param_locations = ["headers", "body", "query", "path"];
+	const def_param_locations = {
+		'POST': 'body',
+		'GET': 'query',
+	};
+
+
+	const serializers = {
+		'multipart/form-data': (function () {
+			var formData = new FormData();
+			formData.toString = function () { return this; };
+			return formData;
+		}),
+		'application/x-www-form-urlencoded': (function () { return new URLSearchParams() }),
+		'application/json': (function () {
+			return {
+				append: function (key, value) { this.data = this.data || {}; this.data[key] = value; },
+				toString: function () { return JSON.stringify(this.data); },
+				isEmpty: function () {
+					return (!this.data || Object.keys(this.data).length == 0)
+				}
+			}
+		}),
+		'text/plain': (function () {
+			return {
+				append: function (_, value) { this.data = this.data || []; return this.data.push(value); },
+				toString: function () { return this.data.join(''); },
+				isEmpty: function () {
+					return (!this.data || this.data.length == 0)
+				}
+			}
+		})
+	};
+
+
+	/**
+	 * Some helper functions
+	 */
+
 	function isNull(value){
 		return value === null || value === undefined;
 	}
@@ -37,115 +103,142 @@ function Wrape(endpoints,global_options){
 	}
 
 
+	/**
+	 * Fetch Wrapper 
+	 * @param {string} url 
+	 * @param {Object} options 
+	 * @param {Function} responseFormatter 
+	 * @return {Promise|*}
+	 */
+	function ajax(url, options, stored_options, response_middleware) {
+		
+		//Custom Sender
+		if(typeof stored_options.sender === "function") {
+			return stored_options.sender(url, options, response_middleware, stored_options);
+		}
 
-
-	/* Fetch Wrapper */
-	function ajax(url, options, responseFormatter) {
 		return fetch(url, options)
-			.then(async function(res){
-				if(global_options.parseResponse){
-					var resobj = {};
-					var content_type = res.headers.get('Content-Type');
-					resobj = {
-						'status_code': res.status,
-						'status': res.status,
-						'statusText': res.statusText,
-						'headers': res.headers,
-					}
+			.then(async function (res) {
+				
+				var resobj = {},
+					do_parse_response = stored_options.fetch_parse,
+					do_format_response = typeof response_middleware === "function" ? response_middleware : null,
+					do_handle_errors = typeof stored_options.fetch_error_handler === "function" ? stored_options.fetch_error_handler : null;
+				
+				try {
+					if(do_parse_response) {
+						var content_type = res.headers.get('Content-Type');
+						resobj = {
+							'status_code': res.status,
+							'status': res.status,
+							'statusText': res.statusText,
+							'headers': res.headers,
+						}
 					
-					
-					try {
 						if(content_type && (new RegExp("application\/json")).test(content_type)) {
 							resobj['json'] = await res.json();
 						}
-						else if(content_type && (new RegExp("text\/")).test(content_type)) { resobj['text'] = await res.text(); }
-						else { resobj['arrayBuffer'] = await res.arrayBuffer() }
-					}
-					catch(err) {
-						console.warn(err);
-					}
+						else {
+							resobj['text'] = await res.text();
+						}
 
-					var default_msg = (res.ok ? "Success." : "Something went wrong.");
-					resobj['message'] = (resobj['json'] && resobj['json']['message']) ? resobj['json']['message'] : default_msg;
-				}
-				else{
-					resobj = res;
-				}
+						var default_msg = (res.ok ? "Success." : "Something went wrong.");
+						resobj['message'] = (resobj['json'] && resobj['json']['message']) ? resobj['json']['message'] : default_msg;
+					}
+					else {
+						resobj = res;
+					}
 				
-				if (!res.ok) {
-					return Promise.reject(resobj);
+					if(!res.ok) {
+						throw new Error("Request failed.");
+					}
 				}
-				if(typeof responseFormatter === "function") {
-					return responseFormatter(resobj);
+				catch(err) {
+					if(do_handle_errors) {
+						return do_handle_errors(err,
+							{
+								url: url,
+								options: options
+							},
+							resobj);
+					}
+					return Promise.reject(err);
 				}
-				return resobj
+
+				return do_format_response ?
+					do_format_response(resobj) :
+					resobj;
 			});
 	}
 	
 
-	//The set constructor
-	function newSetObject(root,name){
+	/**
+	 * Constructor Maker Wrapper
+	 * @param {*} root 
+	 * @param {*} name 
+	 */
+	function newSetObject(root,name, category_options){
 		name = name || "Wrape";
 		var New = {
-			[name]: (function(def_values){
+			[name]: (function(options){
 				if(!(this instanceof New[name])){
 					throw new Error("You must initalize this object using 'new' command.");
 				}
-				Object.assign(this,root);
-				this.def_values = def_values;
-				//Set for the current initalized object
-				var child_setter = (function(def_values){
-					if ((this instanceof child_setter)) { throw new Error("You can't initalize this object.");}
-					this.def_values = def_values;}
-					).bind(this);
-				this.set = child_setter;
+				Object.assign(this, root);
 				
-				
+				this.set = (function (options) {
+					this.$options = Object.assign({}, global_options, category_options, options);
+				}).bind(this);
+
+				this.set(options);
+
 				return this;
 			})
 		}
 		return New[name];
 	}
 	
-	
-	
-
-	//Main Part
-	function fetcher(request){
+	/**
+	 * Main Wrapper
+	 * @param {Object} request 
+	 */
+	function fetcher(request, category_options){
 		
 		request.method = (typeof request.method === "string" ? request.method.toUpperCase() : "GET");
 		
+		request.enctype = def_param_enctypes[request.enctype] || (allowed_param_enctypes.includes(request.enctype) ? request.enctype : allowed_param_enctypes[0]);
 
-		//Request Body Encoding (if any)
-		var shorthand_enctypes = {
-			"form": "multipart/form-data",
-			"url": "application/x-www-form-urlencoded",
-			"json": "application/json"
+		if(category_options) {
+			category_options = Object.assign({}, global_options, category_options);
 		}
-		var allowed_param_enctypes = Object.values(shorthand_enctypes);
-		request.enctype = shorthand_enctypes[request.enctype] || (allowed_param_enctypes.includes(request.enctype) ? request.enctype : allowed_param_enctypes[0]);
 
+		return async function (params) {
+		
+			var stored_options = this.$options || category_options || global_options ;
 
-		var allowed_param_locations = ["headers", "body", "query", "path"];
-		var def_param_locations = {
-			'POST': 'body',
-			'GET': 'query',
-		}
-		
-		
-		return async function(params){
-			var url = `${global_options.base}${request.path}`;
+			var url = `${stored_options.base}${request.path}`;
+
 			var options = {
 				method: request.method,
-				headers: global_options.defaultHeaders,
-				agent: global_options.agent
+				headers: stored_options.headers,
+				agent: stored_options.fetch_agent
 			}
 			
 			
 			var request_params = Object.keys(request.params);
 			var argument_params = Array.from(arguments);
+
 			
-			//Parse the params object
+			var request_middleware = request.request || stored_options.request_middleware;
+			var response_middleware = request.response || stored_options.response_middleware;
+			
+			var bodySerializer = serializers[request.enctype](),
+				querySerializer = new URLSearchParams();
+			
+			
+			/**
+			 * Parse Params
+			 */
 			
 			//Support passing multiple arguments, create param object ordered by param order. (Not recommended)
 			if(argument_params.length > 1){
@@ -174,25 +267,9 @@ function Wrape(endpoints,global_options){
 				params = params || {};
 			}
 			
-	
-			this.def_values = this.def_values || {};
-			var bodyTypes = {
-				'multipart/form-data': function(){ return new FormData();},
-				'application/x-www-form-urlencoded': function(){return new URLSearchParams()},
-				'application/json': function(){
-					return {
-						append: function(key,value){ return this[key] = value;},
-						toString: function(){ return JSON.stringify(this);}
-					}
-				}
-			}
-			var body = bodyTypes[request.enctype]();
-			var query = new URLSearchParams();
-			
-	
 			for (var param_name in request.params){
 				var param = request.params[param_name];
-				var param_value = params[param_name] || this.def_values[param_name] || global_options.defaultParamValues[param_name] || param.default;
+				var param_value = params[param_name] || param.default || stored_options.values[param_name];
 				var param_dest = param.name || param_name;
 				
 				//Required Param or not
@@ -229,26 +306,32 @@ function Wrape(endpoints,global_options){
 					continue;
 				}
 				if(param_location == "body"){
-					body.append(param_dest,param_value);
+					bodySerializer.append(param_dest,param_value);
 					continue;
 				}
 				if(param_location == "query"){
-					query.append(param_dest,param_value);
+					querySerializer.append(param_dest,param_value);
 					continue;
 				}
 				if(param_location == "path"){
 					url = url.replace(new RegExp(`\:${escapeRegExp(param_name)}`),param_value);
 				}
 			}
-			var hasQuery = query.toString();
+
+			//Set Query
+			var hasQuery = querySerializer.toString();
 			
 			if(hasQuery){
 				url = `${url}?${hasQuery}`;
 			}
 			
-			//Get Body
-			if((body.keys && !isEmptyIterable(body.keys())) || (request.enctype == allowed_param_enctypes[2] && body.toString())){
-				options['body'] = ((request.enctype == allowed_param_enctypes[0]) ? body : body.toString());
+			//Set Body
+			var isEmptyBody = (bodySerializer.keys && isEmptyIterable(bodySerializer.keys())) ||
+				(bodySerializer.getLengthSync && bodySerializer.getLengthSync() == 0) ||
+				(bodySerializer.isEmpty && bodySerializer.isEmpty());
+
+			if(!isEmptyBody){
+				options['body'] = bodySerializer.toString();
 			}
 			
 			//Set content-type header, (not set for multipart/form-data because it overrides the multpart key)
@@ -256,17 +339,47 @@ function Wrape(endpoints,global_options){
 				options['headers'] = options['headers'] || {};
 				options['headers']['Content-Type'] = request.enctype;
 			}
-	
-			return ajax(url,options, request.response);
+
+			//Pre-Request Middleware
+			if(typeof request_middleware === "function") {
+				var override = await Promise.resolve(request_middleware(url,
+					options,
+					querySerializer,
+					bodySerializer));
+				
+				if(override === false) {
+					return false;
+				}
+
+				if(override && override.url) {
+					url = override.url;
+				}
+				
+				if(override && override.options) {
+					options = returns.options;
+				}
+			}
+
+			
+			return ajax(url, options, stored_options, response_middleware);
 		}
 	}
 	
 
-	//Recursive Function
-	function ocreater(root,tree,name){
+	/**
+	 * Recursive Loop schema and make wrappers
+	 * @param {*} root 
+	 * @param {*} tree 
+	 * @param {*} name 
+	 */
+	function ocreater(root, tree, name, category_options){
 		for(var category in tree){
 			var category_tree = tree[category];
 			
+			if(!category_tree || typeof category_tree !== 'object') {
+				continue;				
+			}
+
 			//Is Endpoint
 			if(category_tree.hasOwnProperty('path')){
 				var request = category_tree;
@@ -277,22 +390,31 @@ function Wrape(endpoints,global_options){
 						console.warn(`Skipping ${category} as it confilicts with another key in the object, avoid using keys like ('set') because they are reserved constructor words.`);
 						continue;
 				}
-				root[category] = fetcher(request);
+				root[category] = fetcher(request, category_options);
 		
 			}
 			//Is Category, recursion
-			else{
-				root[category] = ocreater({},category_tree,category)
+			else {
+				
+				if(category === '$options') { //Special Options
+					category_options = category_tree;
+				}
+
+				root[category] = category.substr(0, 1) === '$' ? //Special Object (i.e Options)
+					category_tree :
+					ocreater({}, category_tree, category, category_options)
 			}
 		}
 		
 		//If it has endpoints , add the 'set' constructor function.
-		root = (root._ne) ? Object.assign(root,{'set': newSetObject(root,name)}) : root;
+		root = (root._ne) ? Object.assign(root,{'set': newSetObject(root,name, category_options)}) : root;
 		
 		return root;
 	}
+
 	return ocreater({},endpoints,true);
 }
+
 
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
@@ -303,5 +425,17 @@ function Wrape(endpoints,global_options){
         root.Wrape = factory();
   }
 }(typeof self !== 'undefined' ? self : this, function () {
-    return Wrape;
+	const fetch = (typeof module === 'object' && module.exports) ? require("node-fetch") : window.fetch;
+	const FormData = (typeof module === 'object' && module.exports) ? require("form-data") : window.FormData;
+
+	if(!fetch) {
+		console.warn("Fetch API is not installed. If you are using Node please install node-fetch.");
+		return false;
+	}
+	if(!FormData) {
+		console.warn("FormData is not installed. If you are using Node please install form-data.");
+		return false;
+	}
+	
+    return Wrape.bind(null,fetch,FormData);
 }));
